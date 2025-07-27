e58b9a0d29668ec70b1cdcb25255a9564cc756e6
Sitnyasha <igorsit96@gmail.com>
2025-07-27 19:23:44 +0300

    123

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-import java.net.SocketOption;
 import java.util.Scanner;
 
-public class Main  {
-
+class KeyboardNeighbor {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
-        do
+        System.out.print("Введите букву английского алфавита: ");
+        String input = scanner.nextLine().toLowerCase();
+
+        if (input.length() != 1 || !Character.isLetter(input.charAt(0))) {
+            System.out.println("Ошибка: нужно ввести одну букву!");
+            return;
+        }
+
+        char letter = input.charAt(0);
+        char leftNeighbor = findLeftNeighbor(letter);
+
+        System.out.println("Буква слева: " + leftNeighbor);
     }
-    public static void dummy() {
-        System.out.println("");
+
+    public static char findLeftNeighbor(char letter) {
+        String keyboard = "qwertyuiopasdfghjklzxcvbnm";
+
+        int index = keyboard.indexOf(letter);
+        if (index == -1) {
+            return '?';
+        }
+
+        if (index == 0) {
+            return keyboard.charAt(keyboard.length() - 1);
+        } else {
+            return keyboard.charAt(index - 1);
+        }
     }
 }
